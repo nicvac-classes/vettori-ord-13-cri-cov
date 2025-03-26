1a042ffe08768f901bb745e46ea17751d77d576e
cri-cov <cristian.covato@marconibari.edu.it>
2025-03-26 18:04:09 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,51 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int n, i, j, t;
+    bool scambio;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    cout << "quanti numeri vuoi ordinare?" << endl;
+    cin >> n;
+    int num[n];
+
+    i = 0;
+    scambio = true;
+    while (i < n) {
+        num[i] = rand() % (10 * n);
+        i = i + 1;
+    }
+    i = 0;
+    while (i <= n - 1 && scambio) {
+        scambio = false;
+        j = 0;
+        while (j <= n - 2 - i) {
+            if (num[j] > num[j + 1]) {
+                t = num[j];
+                num[j] = num[j + 1];
+                num[j + 1] = t;
+                scambio = true;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    i = 0;
+    while (i < n) {
+        if (num[i] % 2 == 0) {
+            cout << "il numero pari è " << j + 1 << " & num[i]" << endl;
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    i = 0;
+    j = 0;
+    while (i < n) {
+        if (num[i] % 2 != 0) {
+            cout << "il numero pari è " << j + 1 << " & num[i]" << endl;
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    
+
+
